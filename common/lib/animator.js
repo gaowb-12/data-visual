24d83734ce9d498cf80476181b61c4410d015b9b
gaowb <gaowb_12@163.com>
2021-08-05 08:49:04 +0800

    简单动画-三种实现动画的方式

---
+++ common/lib/animator.js
@@ -0,0 +1,23 @@
+import {Timing} from './timing.js';
+
+class Animator {
+    constructor({duration, interations}){
+        this.timing = {duration, interations}
+    }
+    animate(target, update){
+        let frameIndex = 0;
+        const timing = new Timing(this.timing);
+
+        return new Promise((resolve)=>{
+            function next(){
+                if(update({target, frameIndex, timing})!==false && !timing.isFinished){
+                    requestAnimationFrame(next);
+                }else{
+                    resolve(timing);
+                }
+                frameIndex++;
+            }
+            next();
+        })
+    }
+}
